50cbd2d96f1290660ef433ee436e47c3bd5b7902
zouhl <zouhl0924@gmail.com>
2023-09-26 19:43:17 +0800

    提交第5篇文章的代码

---
+++ SpringCircularReferenceDemo/src/main/java/com/homura/app/config/AppConfig.java
@@ -0,0 +1,9 @@
+package com.homura.app.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@ComponentScan(value = {"com.homura.app"})
+@Configuration
+public class AppConfig {
+}
